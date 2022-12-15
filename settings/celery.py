1d9a5a5644ba259ca65056bd02ddb8ffcff937fb
btk_server <btk.kg@bk.ru>
2022-12-15 18:54:41 +0000

    fix: Устранено ошибка имени в celery на сервере

--- settings/celery.py
+++ settings/celery.py
@@ -4,7 +4,7 @@ from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.settings')
 
-app = Celery('MedService')
+app = Celery('settings')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
