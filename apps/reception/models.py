7d8a19eadec01a262c4c79605a87c01ea1831ee2
Suvan <overmind77s@gmail.com>
2022-12-19 13:54:06 +0600

    create Reception model

--- apps/reception/models.py
+++ apps/reception/models.py
@@ -14,3 +14,28 @@ class Date_reception(models.Model):
     time = models.TimeField()
 
 
+class Reception(models.Model):
+
+    patient = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name='Patient',
+        related_name='patients',
+        on_delete=models.CASCADE
+    )
+    doctor = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name='Doctor',
+        related_name='doctors',
+        on_delete=models.CASCADE
+    )
+    description = models.CharField(
+        'Text',
+        max_length=300,
+        null=True
+    )
+    date_reception = models.ForeignKey(
+        'Date_reception',
+        on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField('Created_time', default=now)
+    updated_at = models.DateTimeField('Update_time', blank=True, null=True)
