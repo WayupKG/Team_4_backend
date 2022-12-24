b7ecddb017de18af1cc62e9a7cf2496049ee61ef
wayupkg <adikgk232323@gmail.com>
2022-12-24 10:27:56 +0600

    fix: Исправлено models.py

--- apps/reception/models.py
+++ apps/reception/models.py
@@ -1,41 +1,39 @@
 from django.db import models
-from settings import settings
+from django.contrib.auth import get_user_model
 
-
-class Date_reception(models.Model):
-
-    doctor = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        verbose_name='Doctor',
-        on_delete=models.CASCADE
-    )
-    date = models.DateField()
-    time = models.TimeField()
+User = get_user_model()
 
 
 class Reception(models.Model):
+    patient = models.ForeignKey(to=User, verbose_name='Patient',
+                                related_name='my_receptions', on_delete=models.CASCADE)
+    doctor = models.ForeignKey(to=User, verbose_name='Doctor', related_name='receptions',
+                               on_delete=models.CASCADE)
+    description = models.TextField('Description')
+    date_reception = models.ForeignKey('DateReception', on_delete=models.CASCADE)
+    created_at = models.DateTimeField('Created time', auto_now_add=True)
+    updated_at = models.DateTimeField('Updated time', auto_now=True)
 
-    patient = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        verbose_name='Patient',
-        related_name='patients',
-        on_delete=models.CASCADE
-    )
-    doctor = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        verbose_name='Doctor',
-        related_name='doctors',
-        on_delete=models.CASCADE
-    )
-    description = models.CharField(
-        'Text',
-        max_length=300,
-        blank=True,
-        null=True
-    )
-    date_reception = models.ForeignKey(
-        'Date_reception',
-        on_delete=models.CASCADE
-    )
-    created_at = models.DateTimeField('Created_time', auto_now_add=True)
-    updated_at = models.DateTimeField('Update_time', auto_now=True)
+    class Meta:
+        verbose_name = 'Reception'
+        verbose_name_plural = 'Receptions'
+
+    def __str__(self):
+        return f"{self.date_reception} -> {self.patient}"
+
+
+class DateReception(models.Model):
+    doctor = models.ForeignKey(to=User, verbose_name='Doctor', on_delete=models.CASCADE,
+                               related_name='date_receptions')
+    date = models.DateField()
+    time = models.TimeField()
+
+    class Meta:
+        verbose_name = 'Date reception'
+        verbose_name_plural = 'Date receptions'
+
+    def __str__(self):
+        return f"{self.doctor}: {self.date} -> {self.time}"
+
+
+
